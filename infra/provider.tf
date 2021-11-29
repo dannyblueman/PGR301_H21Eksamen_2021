bb94f38b7effbed753acde1bcebd38c6e602f1ab
dannyblueman <dhb_90@hotmail.com>
2021-11-29 20:05:30 +0100

    Pipeline code

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,4 +5,9 @@ terraform {
       version = "3.56.0"
     }
   }
+  backend "s3" {
+    bucket = "pgr301-danib-terraform"
+    key    = "glennbech/06-terraform-intro.state"
+    region = "eu-west-1"
+  }
 }
